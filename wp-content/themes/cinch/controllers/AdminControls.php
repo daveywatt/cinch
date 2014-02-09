e125a04e23ca879ed81b8dbac1d0cd6de0daa57c
Dave <daveywatt@gmail.co>
2014-02-10 09:04:14 +1100

    Admin menu API progress

--- wp-content/themes/cinch/controllers/AdminControls.php
+++ wp-content/themes/cinch/controllers/AdminControls.php
@@ -2,44 +2,18 @@
 /**
  * Cinch Admin Controls
  * admin-controls.php
- * This class streamlines wordpress admin interface API functions
+ * This class streamlines the WordPress admin interface API functions
  */
 
 class AdminControls extends cinch\Cinch {
 
     private static $scripts;
+    private static $styles;
 
     public function __construct() {
-
     }
 
-    public static function menuPages($menus) {
-
-        /*
-         *  $adminPages = array(
-
-                'Cinch' => array(
-                    'title' => 'Options',
-                    'top_title' => 'Options',
-                    'capability' => ADMIN_CAPABILITY,
-                    'slug' => 'cinch',
-                    'function' => array(CINCH_CLASS, 'adminOptionsPage'),
-                    'icon' => array('css3', 'core'),
-                    'position' => 3,
-                    'scripts' => array('jquery-ui-core', 'jquery-ui-selectable', 'jquery-ui-sortable'),
-                    'submenus' => array(
-                        'cinch' => array(
-                            'page_title' => 'Add-ons',
-                            'menu_title' => 'Add-ons',
-                            'capability' => ADMIN_CAPABILITY,
-                            'slug' => 'cinch-addons',
-                            'function' => array(CINCH_CLASS, 'adminAddonsPage')
-                        )
-                    )
-                )
-            );
-
-         */
+    public static function pages($menus) {
 
         if (!parent::checkValidArray($menus)) return new WP_Error('empty_or_incorrect_datatype', __('Incorrect arguments for admin menu page.'));
 
@@ -48,23 +22,23 @@ class AdminControls extends cinch\Cinch {
             //TODO: configure css icons properly (simply add class to menu maybe?)
             $icon = ((parent::checkValidArray($menu['icon']) && $menu['icon'][0] == 'css3') ? null : $menu['icon']);
 
-            $menuPage = add_menu_page($menu['title'], $label, $menu['capability'], $menu['slug'], $menu['function'], $icon, $menu['position']);
+            $menuPage = add_menu_page($menu['title'], $label, $menu['capability'], $menu['slug'], $menu['callback'], $icon, $menu['position']);
 
             if (parent::checkValidArray($menu['scripts'])) {
-
                 self::$scripts = $menu['scripts'];
-                //TODO: get all parameters for enqueuing scripts/styles
                 add_action('load-'.$menuPage, array('AdminControls', 'enqueueScripts'), 10);
+            }
 
+            if (parent::checkValidArray($menu['styles'])) {
+                self::$styles = $menu['styles'];
+                add_action('admin_enqueue_scripts', array('AdminControls', 'enqueueStyles'));
             }
 
             if (parent::checkValidArray($menu['submenus'])) {
-
                 foreach($menu['submenus'] as $parentSlug => $submenu) {
                     //TODO: attach script/styles enqueuing to submenus
-                    $subMenuPage = add_submenu_page($parentSlug, $submenu['page_title'], $submenu['menu_title'], $submenu['capability'], $submenu['slug'], $submenu['function']);
+                    add_submenu_page($parentSlug, $submenu['page_title'], $submenu['menu_title'], $submenu['capability'], $submenu['slug'], $submenu['function']);
                 }
-
             }
 
             if (parent::checkValidString($menu['top_title'])) {
@@ -73,15 +47,31 @@ class AdminControls extends cinch\Cinch {
             }
 
         }
+    }
 
+    public static function sections($sections) {
+        foreach($sections as $section) {
+            add_settings_section($section['group'], (parent::checkValidString($section['title']) ? $section['title'] : null), $section['view'], $section['page']);
+        }
     }
 
     public static function enqueueScripts() {
 
-        //TODO: full enqueue script paramaters
-        foreach (self::$scripts as $script) wp_enqueue_script($script);
+        foreach (self::$scripts as $script) {
+            if (parent::checkValidString($script['bundled']))  {
+                wp_enqueue_script($script);
+                continue;
+            } else {
+                wp_enqueue_script($script['slug'], $script['source'], $script['dependencies'], $script['version']);
+            }
+        }
     }
 
+    public static function enqueueStyles($hook) {
 
+        foreach (self::$styles as $style) {
+            if (strstr($hook, $style['hook'])) wp_enqueue_style($style['slug'], $style['source'], $style['dependencies'], $style['version']);
+        }
+    }
 
 }
